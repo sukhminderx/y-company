0a37792cab23de8ea8675b4ea9a5bd8499f1505b
sukhminderx <sukhmindersinghparmarsocialnet@gmail.com>
2023-10-05 23:19:11 -0400

    products

---
+++ src/app/search/products/products.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NgOptimizedImage } from '@angular/common';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.scss'],
+  standalone: true,
+  imports: [
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    NgOptimizedImage,
+  ],
+})
+export class ProductsComponent {}
--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Injectable } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -6,6 +6,36 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { FiltersComponent } from './filters/filters.component';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatIconModule } from '@angular/material/icon';
+import { ProductsComponent } from './products/products.component';
+import {
+  MatPaginatorIntl,
+  MatPaginatorModule,
+} from '@angular/material/paginator';
+import { Subject } from 'rxjs';
+
+@Injectable()
+export class MyCustomPaginatorIntl implements MatPaginatorIntl {
+  changes = new Subject<void>();
+
+  // For internationalization, the `` function from
+  // the `@angular/localize` package can be used.
+  firstPageLabel = `First page`;
+  itemsPerPageLabel = `Items per page:`;
+  lastPageLabel = `Last page`;
+
+  // You can set labels to an arbitrary string too, or dynamically compute
+  // it through other third-party internationalization libraries.
+  nextPageLabel = 'Next page';
+  previousPageLabel = 'Previous page';
+
+  getRangeLabel(page: number, pageSize: number, length: number): string {
+    if (length === 0) {
+      return `Page 1 of 1`;
+    }
+    const amountPages = Math.ceil(length / pageSize);
+    return `Page ${page + 1} of ${amountPages}`;
+  }
+}
 
 @Component({
   selector: 'app-search',
@@ -19,7 +49,10 @@ import { MatIconModule } from '@angular/material/icon';
     ReactiveFormsModule,
     FiltersComponent,
     MatExpansionModule,
+    ProductsComponent,
     MatIconModule,
+    MatPaginatorModule,
   ],
+  providers: [{ provide: MatPaginatorIntl, useClass: MyCustomPaginatorIntl }],
 })
 export class SearchComponent {}
