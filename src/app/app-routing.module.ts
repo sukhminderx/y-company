c2d2e55ed17a4a1c2c36877e0a9db5ae8f689eb9
sukhminderx <sukhmindersinghparmarsocialnet@gmail.com>
2023-10-14 14:31:43 -0400

    title

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,11 +7,13 @@ export const routes: Routes = [
     path: '',
     loadComponent: () =>
       import('./home/home.component').then((mod) => mod.HomeComponent),
+    title: 'Y Company - One stop for latest fashion for youth',
   },
   {
     path: 'login',
     loadComponent: () =>
       import('./login/login.component').then((mod) => mod.LoginComponent),
+    title: 'Y Company - Login',
   },
   {
     path: 'register',
@@ -19,37 +21,36 @@ export const routes: Routes = [
       import('./register/register.component').then(
         (mod) => mod.RegisterComponent
       ),
+    title: 'Y Company - Register',
   },
   {
     path: 'search',
     loadComponent: () =>
       import('./search/search.component').then((mod) => mod.SearchComponent),
+    title: 'Y Company - Searching latest fashion products', // need resolve
   },
   {
     path: 'detail',
     loadComponent: () =>
       import('./detail/detail.component').then((mod) => mod.DetailComponent),
+    title: 'Y Company - Product detail', // need resolve
   },
   {
     path: 'cart',
     loadComponent: () =>
       import('./cart/cart.component').then((mod) => mod.CartComponent),
+    title: 'Y Company - your cart is ready',
   },
   {
     path: 'payment',
     loadComponent: () =>
       import('./payment/payment.component').then((mod) => mod.PaymentComponent),
+    title: 'Y Company - payment',
   },
   {
     path: 'paid',
     loadComponent: () =>
       import('./paid/paid.component').then((mod) => mod.PaidComponent),
+    title: 'Y Company - order summary',
   },
 ];
-/*
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
-})
-export class AppRoutingModule {}
-*/
