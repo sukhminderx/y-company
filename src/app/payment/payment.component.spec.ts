86e71cb3f654f927e49a67ff4f1526d06b2c3541
sukhminderx <sukhmindersinghparmarsocialnet@gmail.com>
2023-10-22 01:30:52 -0400

    UT

--- src/app/payment/payment.component.spec.ts
+++ src/app/payment/payment.component.spec.ts
@@ -1,12 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { PaymentComponent } from './payment.component';
-import { BehaviorSubject } from 'rxjs';
+import { of } from 'rxjs';
 import { PaymentService } from './payment.service';
 import { Meta } from '@angular/platform-browser';
-import { Router } from '@angular/router';
+import { Router, provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
+import { PLATFORM_ID } from '@angular/core';
 const paymentService = {
-  getAddress: jasmine.createSpy('getAddress'),
+  getAddress: jasmine.createSpy('getAddress').and.returnValue(of({})),
   doPayment: jasmine.createSpy('doPayment'),
 };
 const meta = {
@@ -16,14 +18,25 @@ const router = {
   navigate: jasmine.createSpy('navigate'),
 };
 describe('PaymentComponent', () => {
-  beforeEach(() =>
+  let fixture: any;
+  let component: any;
+  beforeEach(() => {
+    TestBed.overrideComponent(PaymentComponent, {
+      set: {
+        providers: [
+          {
+            provide: PaymentService,
+            useValue: paymentService,
+          },
+        ],
+      },
+    });
     TestBed.configureTestingModule({
       imports: [RouterTestingModule, PaymentComponent],
       providers: [
-        {
-          provide: PaymentService,
-          useValue: paymentService,
-        },
+        { provide: PLATFORM_ID, useValue: 'browser' },
+        provideHttpClient(),
+        provideRouter([]),
         {
           provide: Meta,
           useValue: meta,
@@ -33,12 +46,45 @@ describe('PaymentComponent', () => {
           useValue: router,
         },
       ],
-    })
-  );
+    });
+
+    fixture = TestBed.createComponent(PaymentComponent);
+    component = fixture.componentInstance;
+  });
 
   it('should create the component', () => {
-    const fixture = TestBed.createComponent(PaymentComponent);
-    const component = fixture.componentInstance;
     expect(component).toBeTruthy();
+  });
+
+  it('should test updateTag', () => {
+    component.ngOnInit();
+    expect(meta.updateTag).toHaveBeenCalled();
+  });
+
+  it('should test isLoggedIn', () => {
+    spyOn(localStorage, 'getItem').and.callFake((key: string): string => {
+      return 'test';
+    });
+    component.ngOnInit();
+    expect(component.isLoggedIn).toBeTrue();
+    expect(paymentService.getAddress).toHaveBeenCalled();
+  });
+
+  it('should test pay', () => {
+    component.form.patchValue({
+      email: 'qwerty@gmail.com',
+      name: 'qwerty',
+      address: 'qwerty',
+      phone: 'qwerty',
+      city: 'qwerty',
+      state: 'qwerty',
+      pincode: 'qwerty',
+      cardNumber: '2222222222222222',
+      month: 'qwerty',
+      year: 'qwerty',
+      cvv: '233',
+    });
+    component.pay();
+    expect(paymentService.doPayment).toHaveBeenCalled();
   });
 });
